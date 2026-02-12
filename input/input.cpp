2d08a4b95c0303d8faa8d7a17722e121ea73fe65
Methas <methas_n@cmu.ac.th>
2026-02-12 22:31:53 +0700

    "Edited condition"

--- input/input.cpp
+++ input/input.cpp
@@ -1,7 +1,84 @@
 #include<iostream>
+#include<limits>
 #include "input.h"
 
 using namespace std;
+
+void displayBanner(){
+
+    cout << endl<<"===================================="<<endl;
+    cout << endl<< "    SMART TRIP PLANNER SYSTEM      "<<endl;
+    cout << endl<<"===================================="<<endl;
+
+}
+
+void clearInputBuffer(){
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');//reset error state ลบทุกอย่างจนกว่าจะเจอ newline
+}
+
+int getValid_Intiger(const string &prompt){
+    int value;
+
+    while(1){
+        cout << prompt;
+        cin >> value;
+        
+        //check ว่าเป็น Number มั้ย
+        if(value < 0){
+            cout << "Error : Please enter a valid number or number more than zero. \n" ;
+            clearInputBuffer(); //ล้างสิ่งที่ผู้ใช้ป้อนค้างไว้ใน Buffer
+            continue;
+        }
+
+        clearInputBuffer(); //clear for next input
+        return value;
+    }
+
+}
+
+// get input type Yes/No แปลงเป็น Boolean
+bool getYes_No_Input(const string &prompt){
+    string input_YN;
+
+    while(1){
+        cout << prompt << "[Yes / No] : ";
+        getline(cin,input_YN);
+
+        for(char &text : input_YN){
+            text = toupper(text);
+        }
+        
+        if(input_YN == "Yes" || "y" || "yes"){
+            return true;
+        }else if(input_YN == "No" || "n" || "no"){
+            return false;
+        }else {
+            cout << "Error: Please enter 'yes' or 'no'.\n";
+        }
+
+    }
+
+}
+
+string getString_Input(const string& prompt){
+    string str;
+    string g;
+
+    while(1){
+        cout << prompt;
+        getline(cin,str);
+
+        if(!str.empty()){
+            return str;
+        }else{
+            cout << "Error : Input can't be empty"<<endl;
+        }
+    }
+
+}
+
+
 
 TravelPreference getUserInput(){
 
@@ -26,8 +103,5 @@ TravelPreference getUserInput(){
 
     cout << "Preferred travel style (Relax / Adventure / Foodie / Photography): ";
     getline(cin,pref.tripStyle);
-
-
-
 
 }
