a9cc1aeef6fe4792d66a85d1fd874a2f3cffbe64
Methas <methas_n@cmu.ac.th>
2026-02-12 22:31:18 +0700

    "Add Function"

--- input/input.h
+++ input/input.h
@@ -1,7 +1,34 @@
 #ifndef INPUT_H
 #define INPUT_H
 
+#include<string>
+using namespace std;
 
-//test
+struct TravelPreference{
+    int budget; //งบ
+    int days; // จน.วันที่ไป
+    int people; // จน.คนที่ไป
+
+    bool hascar; // มีรถบ่
+    string tripStyle; //ชอบเที่ยวแบบไหนหงะ
+
+
+};
+
+TravelPreference getUserInput(); //Prototype of function input #1.
+
+//help for validation
+int getValid_Intiger(const string &prompt);
+bool getYes_No_Input(const string &prompt);
+string getString_Input(const string &prompt);
+
+
+//Show UI
+void displayBanner();
+void clearInputBuffer();
+
+//Error
+void Error();
+
 
 #endif
